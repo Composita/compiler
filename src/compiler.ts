bc1d6952a4d3700c701d0daa6a408599c3ca8204
Hansruedi Patzen <hp@revy.ch>
2020-07-09 10:45:32 +0200

    compiler: udpate compiler dependencies

--- src/compiler.ts
+++ src/compiler.ts
@@ -3,18 +3,23 @@ import { Parser } from '@composita/parser';
 import { ProgramNode } from '@composita/ast';
 import { IL } from '@composita/il';
 import { SymbolTable } from '@composita/symbols';
-import { CompilerDiagnosis } from '@composita/diagnosis';
+import { CompilerDiagnosis, Diagnosis } from '@composita/diagnosis';
 import { Checker } from '@composita/checker';
 import { Generator } from '@composita/generator';
 
 export class Compiler {
     async compile(uri: string, code: string): Promise<IL> {
-        const lexer: Lexer = new Lexer(new CompilerDiagnosis(), uri, code);
-        const parser: Parser = new Parser(lexer);
+        const diagnosis: Diagnosis = new CompilerDiagnosis();
+        const lexer: Lexer = new Lexer(diagnosis, uri, code);
+        const parser: Parser = new Parser(diagnosis, lexer);
         const ast: ProgramNode = await parser.parse();
+        if (diagnosis.hasErrors()) {
+            diagnosis.print(console.log);
+            throw new Error('Error during Lexing and/or Parsing. See console.log.');
+        }
         const checker: Checker = new Checker();
         const symbols: SymbolTable = await checker.check(ast);
         const generator: Generator = new Generator();
-        return await generator.generate(symbols);
+        return generator.generate(symbols);
     }
 }
